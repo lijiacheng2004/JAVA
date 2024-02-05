7a9009c497630c51efeb80c17d1feb76d757f366
lijiacheng2004 <142208238+lijiacheng2004@users.noreply.github.com>
2024-02-05 17:13:21 +0800

    Initial commit

---
+++ chapter02/src/BiaoShiFu.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;/**
+* project(工程)-module(模块)-package(包)-class(类)
+* ClassName: biaoshifu
+* Package: PACKAGE_NAME
+* Description:
+* @Author Lee JiaCheng
+* @Create 2023/10/21 18:21
+* @Version 1.0
+*/public class biaoshifu {
+}
